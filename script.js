94e01014daf99eea8a97e0e205d3165120c457d0
programmerArif <arifulislamwasim2017.com>
2021-06-25 10:24:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const searchButton = document.getElementById("search-button");
+const mealDetails = document.getElementById("meal-details");
+const notFound = document.querySelector("#not-found");
+const mealList = document.getElementById("meal-list");
+
+searchButton.addEventListener("click", () => {
+    searchValue = document.getElementById("search-bar").value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
+        .then((response) => response.json())
+        .then((json) => showMeals(json.meals))
+        .catch((error) => console.log(error));
+});
+
+
+const showMeals = (meals) => {
+
+    //---Resetting the values---
+    mealList.innerHTML = "";
+    mealDetails.innerHTML = "";
+    mealDetails.style.display = "none";
+    notFound.innerHTML = "";
+    //-------------------------
+
+    //---If meals not found---
+    if (meals == null) {
+        return showNotFound();
+    }
+    //------------------------
+
+    meals.forEach(meal => {
+        let singleMeal = document.createElement("div");
+        singleMeal.innerHTML =
+            `
+            <div class="col">
+                <div class="card text-white bg-dark text-center h-100">
+                    <img src=${meal.strMealThumb} class="card-img-top" alt=${meal.strMeal}>
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                    </div>
+                </div>
+            </div>
+        `;
+        singleMeal.setAttribute("class", "single-meal")
+        mealList.appendChild(singleMeal)
+
+        //---Details about a specific meal item---
+        singleMeal.addEventListener("click", () => {
+            mealDetails.style.display = "block";
+            mealDetails.innerHTML =
+                `
+                <div class="text-white bg-dark w-75 h-50 rounded shadow-lg mb-4 d-flex flex-row">
+                    <img class="w-50 rounded" src=${meal.strMealThumb} alt=${meal.strMeal} class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <ul>
+                            <li>${meal.strIngredient1}</li>
+                            <li>${meal.strIngredient2}</li>
+                            <li>${meal.strIngredient3}</li>
+                            <li>${meal.strIngredient4}</li>
+                            <li>${meal.strIngredient5}</li>
+                            <li>${meal.strIngredient6}</li>
+                            <li>${meal.strIngredient7}</li>
+                        </ul>
+                    </div>
+                </div>
+            `;
+            singleMeal.setAttribute("class", "meal-details-style")
+        })
+        //--------------------------------------------------------
+    });
+}
+
+const showNotFound = () => {
+    notFound.innerHTML = `<h1 class="not-found">Items not found. Search again</h1>`;
+}
